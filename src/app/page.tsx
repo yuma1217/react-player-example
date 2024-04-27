5952e1aa1623aa8028a7937b3a0cb6c6011e19d5
yu-matsumura <29217415+yuma1217@users.noreply.github.com>
2024-04-27 14:21:02 +0900

    Add ローカルファイルの再生

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,5 +6,10 @@ export default function Home() {
   const [isClient, setIsClient] = React.useState(false);
   useEffect(() => setIsClient(true), []);
   if (!isClient) return null;
-  return <ReactPlayer url="https://www.youtube.com/watch?v=LXb3EKWsInQ" />;
+  return (
+    <div>
+      <p>動画</p>
+      <ReactPlayer url="/free_sample_video.mp4" />
+    </div>
+  );
 }
